9b53f3241e2920a01d3e44f0c9b60789d830e0ca
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-11-24 09:22:12 +0700

    post and get

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = reuqire("express");
+const bodyParser = require("body-parser");
+import { randomBytes } from "crypto";
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/posts", (req, res) => {});
-app.get("/posts", (req, res) => {});
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log("Listening on 4000");
